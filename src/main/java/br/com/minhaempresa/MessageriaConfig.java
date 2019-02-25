d4495670ff806c3f5e6e4a4670546c04a87b2e59
leandro.alcantara <leandro.alcantara@mprj.mp.br>
2019-02-25 15:44:42 -0300

    Retornando para configuracao com o activemq

--- src/main/java/br/com/minhaempresa/MessageriaConfig.java
+++ src/main/java/br/com/minhaempresa/MessageriaConfig.java
@@ -3,18 +3,15 @@ package br.com.minhaempresa;
 import br.com.minhaempresa.messageria.RecuperarDocumentoMessageConverter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
 import org.springframework.jms.support.converter.MessageConverter;
-import org.springframework.jndi.JndiObjectFactoryBean;
-import org.springframework.jndi.JndiTemplate;
 
 import javax.jms.ConnectionFactory;
-import javax.naming.Context;
-import javax.naming.NamingException;
-import java.util.Properties;
 
 @Configuration
 @EnableJms
@@ -24,38 +21,12 @@ public class MessageriaConfig {
     private static final Logger Logger = LoggerFactory.getLogger(MessageriaConfig.class);
 
     @Bean
-    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory() {
-        DefaultJmsListenerContainerFactory factory
-                = new DefaultJmsListenerContainerFactory();
-        factory.setConnectionFactory(jmsConnectionFactory());
+    public JmsListenerContainerFactory<?> processoEletronicoMessageFactory(ConnectionFactory connectionFactory,
+                                                                           DefaultJmsListenerContainerFactoryConfigurer configurer) {
+        Logger.info("Criando JMS Message Factory...");
+        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+        configurer.configure(factory, connectionFactory);
         return factory;
-    }
-
-    @Bean
-    public JndiTemplate jndiTemplate() {
-        JndiTemplate jndiTemplate = new JndiTemplate();
-        Properties properties = new Properties();
-        properties.put(Context.INITIAL_CONTEXT_FACTORY,"org.jboss.naming.remote.client.InitialContextFactory");
-        properties.put(Context.PROVIDER_URL,"remote://localhost:4447");
-        properties.put("remote.connectionprovider.create.options.org.xnio.Options.SSL_ENABLED", "false");
-        properties.put("remote.connection.default.connect.options.org.xnio.Options.SASL_POLICY_NOANONYMOUS", "false");
-        properties.put(Context.SECURITY_PRINCIPAL,"teste");
-        properties.put(Context.SECURITY_CREDENTIALS,"jbo$$122333");
-        jndiTemplate.setEnvironment(properties);
-        return jndiTemplate;
-    }
-
-    @Bean
-    public ConnectionFactory jmsConnectionFactory() {
-        JndiObjectFactoryBean objectFactoryBean = new JndiObjectFactoryBean();
-        objectFactoryBean.setJndiTemplate(jndiTemplate());
-        objectFactoryBean.setJndiName("java:jms/RemoteConnectionFactory");
-        try {
-            objectFactoryBean.afterPropertiesSet();
-        } catch (NamingException e) {
-            e.printStackTrace();
-        }
-        return (ConnectionFactory) objectFactoryBean.getObject();
     }
 
     @Bean
